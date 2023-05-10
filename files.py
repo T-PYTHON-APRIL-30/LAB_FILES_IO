47cfa140b727641867c556aa9ca611ad40000d81
juhaina <jh75n.awad@gmail.com>
2023-05-10 23:40:18 +0300

    solve!

---
+++ files.py
@@ -0,0 +1,17 @@
+file = open('to_do.txt ', "a+", encoding="utf-8")
+while True:
+    answer_user = input(f"Do You want to add a new To-Do item? answer by 'y' for yes 'n' for no 'exit' for exit ")
+    if answer_user.lower() == "y":
+        todolist = input("write your note")
+        file.write(todolist+"\n")
+    elif answer_user.lower() == "n":
+     viewlist = input("do you want to list your To-Do items ? ")
+     if answer_user =="y":
+      file.seek(0)
+      content = file.readlines()
+      for line in content:
+         print(f"- {line}")
+    elif answer_user.lower() == "exit":
+        file.close
+        break
+print("thank you for using the To-Do program, come back again soon")
---
+++ lamda.py
@@ -0,0 +1,8 @@
+multuplay = lambda x,y : x+y # multyply اللحين صار عباره ع دالة
+#result = print(multuplay(7,7))
+#print(result)
+print(multuplay(7,7))
+
+
+name = ["Juhaina", "Sammar","Abdullah"]
+name 
