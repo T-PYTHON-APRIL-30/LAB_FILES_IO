7aeebd0e12ef266dd8c6b139213d31a17cd72901
SulimanBinM <sbinmohammed_1@icloud.com>
2023-05-11 14:32:48 +0300

    Fix line 4 & 5.

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
 print("\----- Welcome to your To-Do-List -----/")
 
 while True :
-    file_read = open("toDo_list.txt", "r", encoding="utf-8")
-    file_write = open("toDo_list.txt", "a+", encoding="utf-8")
+    file = open("toDo_list.txt", "a+", encoding="utf-8")    
     print("--------- This is the main page ---------")
     userInput = str(input("'s' -> show the tasks\n'a' -> add new task \n'e' -> Exit \nEnter: ")).lower()
     if not userInput.isdigit() and len(userInput) == 1 :
-        if userInput == "p" :
+        if userInput == "s" :
             print("--------- Your To-Do-List is ---------")
-            content = file_read.read()
-            print("-",content)
+            file.seek(0)
+            content = file.read()
+            print(content)
         elif userInput == "a" :
             newTask = input("Enter your new task: ")
-            file_write.write(newTask+"\n")
+            file.write(f"- {newTask}\n")
             print(f"--------- {newTask} is added ---------")
         elif userInput == "e" :
             print("Good bye :)")
-            file_read.close()
-            file_write.close()
             break
         else:
             print("Wrong entry.. Try again !")
     else:
         print("Wrong enters.. Try again !")
+file.close()
